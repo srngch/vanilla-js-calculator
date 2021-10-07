3f7158f8db47235cdaf5352ed4d49123023464f7
srngch <srngch.dev@gmail.com>
2021-10-07 22:00:26 +0900

    chore: Update webpack config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,33 @@
 const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+
+module.exports = (env, argv) => {
+  let entryPath =
+    argv.mode === 'production'
+      ? ['./public/js/main.js']
+      : ['./src/js/main.js'];
+  return {
+    target: 'web',
+    entry: entryPath,
+    devtool: 'inline-source-map',
+    devServer: {
+      static: './dist',
+      hot: true,
+    },
+    output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist'),
+      clean: {
+        keep: /index.html/,
+      },
+    },
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+      ],
+    },
+  };
 };
