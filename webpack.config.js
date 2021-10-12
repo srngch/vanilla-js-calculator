e2c5759610593ad9ea0be733c2fcc6a8ecb8f0a9
srngch <srngch.dev@gmail.com>
2021-10-12 19:00:04 +0900

    build: set up automatic distribution of gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,26 @@
 const path = require('path');
 
-
-module.exports = (env, argv) => {
-  let entryPath =
-    argv.mode === 'production'
-      ? ['./dist/js/main.js']
-      : ['./src/js/main.js'];
-  return {
-    target: 'web',
-    entry: entryPath,
-    devtool: 'inline-source-map',
-    devServer: {
-      static: './dist',
-      hot: true,
-    },
-    output: {
-      filename: 'main.js',
-      path: path.resolve(__dirname, 'dist'),
-      clean: {
-        keep: /index.html/,
-      },
-    },
-    module: {
-      rules: [
-        {
-          test: /\.css$/i,
-          use: ['style-loader', 'css-loader'],
-        },
-      ],
-    },
-  };
+module.exports = {
+	target: 'web',
+	entry: './src/js/main.js',
+	devtool: 'inline-source-map',
+	devServer: {
+		static: './dist',
+		hot: true,
+	},
+	output: {
+		filename: 'main.js',
+		path: path.resolve(__dirname, 'dist'),
+		clean: {
+			keep: /index.html/,
+		},
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ['style-loader', 'css-loader'],
+			},
+		],
+	},
 };
