d38f7c39f04173d25984afccc431a5ff019e3e2d
srngch <srngch.dev@gmail.com>
2021-10-12 18:54:08 +0900

    Updates

--- src/js/components/Button.js
+++
@@ -1,9 +0,0 @@
-import Component from '../core/Component.js';
-
-class Button extends Component {
-	template() {
-		'<button></button>'
-	}
-}
-
-export default Button;
--- src/js/main.js
+++
@@ -1,4 +0,0 @@
-import App from './app.js';
-import '../css/style.css';
-
-new App(document.querySelector('#app'));
