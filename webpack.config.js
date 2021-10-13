4087c650a53e5e6e634ce636c49d394625ad77d1
srngch <srngch.dev@gmail.com>
2021-10-14 02:39:08 +0900

    build: add config files for jest

--- webpack.config.js
+++ webpack.config.js
@@ -22,5 +22,19 @@ module.exports = {
 				use: ['style-loader', 'css-loader'],
 			},
 		],
+		loaders: [
+			{ exclude: ['node_modules'], loader: 'babel', test: /\.jsx?$/ },
+			{ loader: 'style-loader!css-loader', test: /\.css$/ },
+			{ loader: 'url-loader', test: /\.gif$/ },
+			{ loader: 'file-loader', test: /\.(ttf|eot|svg)$/ },
+		],
+	},
+	resolve: {
+		extensions: ['', 'js', 'jsx'],
+		modules: [
+			'node_modules',
+			'bower_components',
+			'shared',
+		],
 	},
 };
